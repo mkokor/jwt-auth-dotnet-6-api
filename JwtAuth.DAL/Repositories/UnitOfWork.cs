a3664d4664046feb861964a6a840d0df08198709
mkokor <matijakokorr@gmail.com>
2023-07-11 13:24:22 +0200

    unit of work pattern implemented in data access layer

---
+++ JwtAuth.DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using JwtAuth.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuth.DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public IUserRepository UserRepository { get; private set; }
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            UserRepository = new UserRepository(_appDbContext);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
